995b70d741bcbef11f8bef4dde10f1b2cc6147fb
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2024-03-30 19:19:43 +0000

    feat: room to_dict

--- rentomatic/domain/room.py
+++ rentomatic/domain/room.py
@@ -13,3 +13,17 @@ class Room:
     def from_dict(self, init_dict):
         return self(**init_dict)
 
+    def to_dict(self):
+        # # Option 1: unpythonic
+        # return {
+        #         "code": self.code,
+        #         "size": self.size,
+        #         "price": self.price,
+        #         "longitude": self.longitude,
+        #         "latitude": self.latitude,
+        #         }
+
+        # Option 2:
+        return dataclasses.asdict(self)
+
+
